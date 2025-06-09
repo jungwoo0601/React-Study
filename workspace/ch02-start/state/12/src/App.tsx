45719c1434282d54136afac12309dc332a21b44a
jungwoo0601 <pjw1346799@naver.com>
2025-06-09 17:59:44 +0900

    [docs] State.13

--- workspace/ch02-start/state/12/src/App.tsx
+++ workspace/ch02-start/state/12/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>12 이벤트 핸들러에서 state 값을 여러번 변경했을 때 문제점</h2>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          console.log("클릭 핸들러 시작", count);
+
+          // setter 함수가 호출되는 즉시 리렌더링이 되지 않고 성능 최적화를 위해서 모아 두었다가 한번에 반영됨(배치)
+          // setCount(count + 1); // 0 + 1
+          // setCount(count + 1); // 0 + 1
+          // setCount(count + 1); // 0 + 1
+
+          // 리액트가 콜백 함수의 리턴값을 저장해 두었다가 다음에 호출되는 콜백 함수의 인자로 전달함
+          setCount((num) => num + 1); // 0 + 1
+          setCount((num) => num + 1); // 1 + 1
+          setCount((num) => num + 1); // 2 + 1
+
+          console.log("클릭 핸들러 종료", count);
+        }}
+      >
+        +3
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
