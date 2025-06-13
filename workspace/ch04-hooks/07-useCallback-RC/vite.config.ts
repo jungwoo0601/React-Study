27153af63afcfa8192a5deefbd60f17d3b8e4878
jungwoo0601 <pjw1346799@naver.com>
2025-06-13 15:21:55 +0900

    [docs] react hook

--- workspace/ch04-hooks/07-useCallback-RC/vite.config.ts
+++ workspace/ch04-hooks/07-useCallback-RC/vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler", {}]],
-      },
-    }),
+        plugins: [
+          ["babel-plugin-react-compiler", {}]
+        ]
+      }
+    })
   ],
   resolve: {
     alias: [
@@ -17,4 +19,4 @@ export default defineConfig({
       { find: "@pages", replacement: "/src/pages" },
     ],
   },
-});
+})
